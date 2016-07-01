ece5b235e08198ebac6a83ee207c9f628c8ba432
Harold Cooper <hrldcpr@gmail.com>
2016-07-01 17:16:11 -0400

    simplify time comparisons by using a 'minimum time' far in the past

--- run.py
+++ run.py
@@ -7,10 +7,12 @@ import api
 import database
 
 
-UPDATE_PERIOD = datetime.timedelta(days=7)
+MIN_TIME = datetime.datetime.fromtimestamp(0, datetime.timezone.utc)
 DAY = datetime.timedelta(days=1)
+UPDATE_PERIOD = datetime.timedelta(days=7)
+UNFOLLOW_PERIOD = datetime.timedelta(days=7)
+FOLLOW_PERIOD = datetime.timedelta(seconds=10)
 FOLLOWS_PER_DAY = 100
-FOLLOW_PERIOD = datetime.timedelta(seconds=10)  # TODO minutes=1)
 
 
 def now():
@@ -20,9 +22,9 @@ def now():
 def update_followers(db, user, twitter_id):
     with db, db.cursor() as cursor:
         twitter = database.get_twitter(cursor, twitter_id)
-        updated_time = database.get_twitter_followers_updated_time(cursor, twitter_id)
+        updated_time = database.get_twitter_followers_updated_time(cursor, twitter_id) or MIN_TIME
     logging.info('followers for %s last updated at %s', twitter, updated_time)
-    if updated_time and now() - updated_time < UPDATE_PERIOD: return
+    if now() - updated_time < UPDATE_PERIOD: return
 
     api_cursor = -1  # cursor=-1 requests first page
     while api_cursor:  # cursor=0 means no more pages
@@ -35,19 +37,18 @@ def update_followers(db, user, twitter_id):
             twitter_ids = database.add_twitter_api_ids(cursor, data['ids'])
             database.update_twitter_followers(cursor, twitter.id, twitter_ids)
 
-    if updated_time:  # delete old followers, since they no longer are
-        with db, db.cursor() as cursor:
-            database.delete_old_twitter_followers(cursor, twitter.id, updated_time)
+    with db, db.cursor() as cursor:  # delete followers who weren't seen again
+        database.delete_old_twitter_followers(cursor, twitter.id, updated_time)
 
 
 def follow(db, user, twitter_id):
     logging.info('preparing user %s to follow twitter_id=%d', user, twitter_id)
     with db, db.cursor() as cursor:
-        followed_time = database.get_user_followed_time(cursor, user.id)
+        followed_time = database.get_user_followed_time(cursor, user.id) or MIN_TIME
         follows_count = database.get_user_follows_count(cursor, user.id, now() - DAY)
         twitter = database.get_twitter(cursor, twitter_id)
     logging.info('user %s last followed at %s and has %d follows in the last day', user, followed_time, follows_count)
-    if followed_time and now() - followed_time < FOLLOW_PERIOD: raise Exception('user followed too recently')
+    if now() - followed_time < FOLLOW_PERIOD: raise Exception('user followed too recently')
     if follows_count >= FOLLOWS_PER_DAY: raise Exception('user exceeded follows for the day')
 
     logging.info(api.post(user, 'friendships/create', user_id=twitter.api_id))
