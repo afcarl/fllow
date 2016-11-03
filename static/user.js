66786d81f354456c69820dcca729137c59bd22a2
Harold Cooper <harold@x.st>
2016-11-03 21:19:02 +0000

    include current time in chart

--- static/user.js
+++ static/user.js
@@ -1,14 +1,19 @@
-const datify = (time, i) => ({x: new Date(time*1000), y: i+1});
+function datify(times) {
+    const data = times.map((t, i) => ({x: new Date(t*1000), y: i+1}));
+    // add a dummy value with current time, for better chart range:
+    if (data.length) data.push({x: new Date(), y: data[data.length-1].y})
+    return data;
+};
 
 var chart = new Chartist.Line('.ct-chart', {
   series: [
     {
       name: 'series-1',
-      data: FOLLOWED.map(datify)
+      data: datify(FOLLOWED)
     },
     {
       name: 'series-2',
-      data: UNFOLLOWED.map(datify)
+      data: datify(UNFOLLOWED)
     }
   ]
 }, {
